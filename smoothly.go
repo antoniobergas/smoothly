3c5717c5fc30f19d3f8b8c9df203fac6f020297e
antonio.bergas <anthonyprincedom@hotmail.com>
2022-09-12 22:43:50 +0200

    - Refactor of the functions - Added new flags

--- smoothly.go
+++ smoothly.go
@@ -5,27 +5,60 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
+
+var build bool
+var version bool
+var start bool
+var run bool
+
+func init() {
+	flag.BoolVar(&build, "build", false, "Build a main structure")
+	flag.BoolVar(&version, "version", false, "Outputs Smoothly version")
+	flag.BoolVar(&run, "run", false, "Runs the solution locally with docker")
+	flag.BoolVar(&start, "start", false, "Runs the solution locally with docker")
+}
+
+func runBuild (){
+	fmt.Println("Initializing structure...")
+	file, err := os.Create("Dockerfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File created successfully")
+	defer file.Close()
+}
+
+func runVersion(){
+	fmt.Println("1.0.0")
+}
+
+func runSolution(){
+	cmd := exec.Command("docker", "build", "-t", "your-app", ".")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 
-	init := flag.Bool("init", false, "Initialize a main structure")
-	version := flag.Bool("version", false, "Outputs Smoothly version")
-
 	flag.Parse()
 
-	if *init {
-		fmt.Println("Initializing structure...")
-		file, err := os.Create("Dockerfile")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("File created successfully")
-		defer file.Close()
+	if build {
+		runBuild()
 	}
 
-	if *version {
-		fmt.Println("1.0.0")
+	if version {
+		runVersion()
 	}
 
+	if start {
+		runBuild()
+		runSolution()
+	}
+
+	if run {
+		runSolution()
+	}
 }
